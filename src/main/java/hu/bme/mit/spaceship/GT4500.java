21a3c6c29dfeae56e357c5ac408133cecc995036
wschenbuer <wschenbuer@yahoo.com>
2018-04-26 00:43:08 -0700

    fixed switch statement

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -38,8 +38,8 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-    switch (firingMode) {
-      case SINGLE:
+     if(firingMode==SINGLE) {
+     
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
@@ -74,13 +74,13 @@ public class GT4500 implements SpaceShip {
             // if both of the stores are empty, nothing can be done, return failure
           }
         }
-        break;
+        
 
-      case ALL:
+      
         // try to fire both of the torpedo stores
         //TODO implement feature
 
-        break;
+       
     }
 
     return firingSuccess;
